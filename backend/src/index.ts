033b8fe35ce6c5aaadbc71cc598d6d3852ad75cf
D.Line <onlyredline+github@gmail.com>
2023-10-20 00:17:57 -0700

    Server is running

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log('something going on');
+import express from 'express'
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
