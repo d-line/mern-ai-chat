ee3d30979933c73de23eb0ffeddd6c4a3f7ecb5c
D.Line <onlyredline+github@gmail.com>
2023-11-05 22:57:21 -0800

    format

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -22,15 +22,15 @@ const Logo = () => {
         />
       </Link>{" "}
       <Typography
-          sx={{
-            display: { md: "block", sm: "none", xs: "none" },
-            mr: "auto",
-            fontWeight: "800",
-            textShadow: "2px 2px 20px #000",
-          }}
-        >
-            <span style={{ fontSize: '20px'}}>MERN</span> - GPT
-        </Typography>
+        sx={{
+          display: { md: "block", sm: "none", xs: "none" },
+          mr: "auto",
+          fontWeight: "800",
+          textShadow: "2px 2px 20px #000",
+        }}
+      >
+        <span style={{ fontSize: "20px" }}>MERN</span> - GPT
+      </Typography>
     </div>
   );
 };
