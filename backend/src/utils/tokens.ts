a4279669dab57510d797ec4c3db29d511f3a0683
D.Line <onlyredline+github@gmail.com>
2023-11-05 22:25:13 -0800

    format

--- backend/src/utils/tokens.ts
+++ backend/src/utils/tokens.ts
@@ -2,22 +2,34 @@ import jwt from "jsonwebtoken";
 import { COOKIE_NAME } from "./constants.js";
 import { NextFunction, Request, Response } from "express";
 
-export const createToken = (id: string, email: string, expiresIn: string | number) => {
+export const createToken = (
+  id: string,
+  email: string,
+  expiresIn: string | number
+) => {
   const payload = { id, email };
   const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
   return token;
 };
 
-export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.signedCookies[COOKIE_NAME];
-  if (!token || token.trim() === '') {
-    return res.status(401).json({ message: "ERROR", cause: "Token Not Received" });
+  if (!token || token.trim() === "") {
+    return res
+      .status(401)
+      .json({ message: "ERROR", cause: "Token Not Received" });
   }
   return new Promise<void>((resolve, reject) => {
     return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
       if (err) {
         reject(err.message);
-        return res.status(401).json({ message: "ERROR", cause: "Token Invalid" });
+        return res
+          .status(401)
+          .json({ message: "ERROR", cause: "Token Invalid" });
       } else {
         resolve();
         res.locals.jwtData = success;
@@ -28,24 +40,20 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
 };
 
 export const setAuthCookie = (res: Response, user) => {
-    res.clearCookie(COOKIE_NAME, {
-        path: "/",
-        domain: "localhost",
-        httpOnly: true,
-        signed: true
-    });
-    const token = createToken(
-      user._id.toString(),
-      user.email,
-      "7d"
-    );
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 7);
-    res.cookie(COOKIE_NAME, token, {
-      path: "/",
-      domain: "localhost",
-      httpOnly: true,
-      expires,
-      signed: true
-    });
-}
+  res.clearCookie(COOKIE_NAME, {
+    path: "/",
+    domain: "localhost",
+    httpOnly: true,
+    signed: true,
+  });
+  const token = createToken(user._id.toString(), user.email, "7d");
+  const expires = new Date();
+  expires.setDate(expires.getDate() + 7);
+  res.cookie(COOKIE_NAME, token, {
+    path: "/",
+    domain: "localhost",
+    httpOnly: true,
+    expires,
+    signed: true,
+  });
+};
