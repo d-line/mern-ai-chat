a3eb09d2f7731645e2cf5a9e1fc56fa635fd9119
D.Line <onlyredline+github@gmail.com>
2023-11-05 19:56:25 -0800

    no ID for chat schema needed

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,37 +1,31 @@
-import mongoose from 'mongoose';
-import { randomUUID } from 'crypto';
+import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-        default: randomUUID()
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    }
+  role: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
 });
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    chats: [chatSchema]
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  chats: [chatSchema],
 });
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
