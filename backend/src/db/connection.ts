ce34208576f0ee51068a48690b44d50a38d65190
D.Line <onlyredline+github@gmail.com>
2023-11-05 19:56:06 -0800

    format

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,21 +1,21 @@
-import { connect, disconnect } from 'mongoose';
+import { connect, disconnect } from "mongoose";
 
 async function connectToDatabase() {
-    try {
-        await connect(process.env.MONGODB_URL);
-    } catch(error) {
-        console.log(error);
-        throw new Error('Error connecting to database');
-    }
+  try {
+    await connect(process.env.MONGODB_URL);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error connecting to database");
+  }
 }
 
 async function disconnectFromDatabase() {
-    try {
-        await disconnect();
-    } catch(error) {
-        console.log(error);
-        console.log('Error disconnecting from database');
-    }
+  try {
+    await disconnect();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error disconnecting from database");
+  }
 }
 
 export { connectToDatabase, disconnectFromDatabase };
