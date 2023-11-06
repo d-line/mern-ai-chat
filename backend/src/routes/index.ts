82cde2e5d3acae51a6363aa16f886e225139b275
D.Line <onlyredline+github@gmail.com>
2023-11-05 22:06:25 -0800

    quotes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import userRouter from './userRoutes.js';
-import chatRouter from './chatRoutes.js';
+import { Router } from "express";
+import userRouter from "./userRoutes.js";
+import chatRouter from "./chatRoutes.js";
 
 const appRouter = Router();
-appRouter.use('/user', userRouter);
-appRouter.use('/chat', chatRouter);
+appRouter.use("/user", userRouter);
+appRouter.use("/chat", chatRouter);
 
 export default appRouter;
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,12 +1,21 @@
-import { Router } from 'express';
-import { getAllUsers, userSignup, userLogin, userVerify } from '../controllers/userController.js';
-import { loginValidator, signupValidator, validate } from '../utils/validators.js';
-import { verifyToken } from '../utils/tokens.js';
+import { Router } from "express";
+import {
+  getAllUsers,
+  userSignup,
+  userLogin,
+  userVerify,
+} from "../controllers/userController.js";
+import {
+  loginValidator,
+  signupValidator,
+  validate,
+} from "../utils/validators.js";
+import { verifyToken } from "../utils/tokens.js";
 
 const userRouter = Router();
-userRouter.get('/', getAllUsers);
-userRouter.post('/signup', validate(signupValidator), userSignup);
-userRouter.post('/login', validate(loginValidator), userLogin);
-userRouter.get('/auth-status', verifyToken, userVerify);
+userRouter.get("/", getAllUsers);
+userRouter.post("/signup", validate(signupValidator), userSignup);
+userRouter.post("/login", validate(loginValidator), userLogin);
+userRouter.get("/auth-status", verifyToken, userVerify);
 
 export default userRouter;
